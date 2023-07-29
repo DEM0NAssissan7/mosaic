437f2581853c5cbfa8e9943628f9dfd46b612048
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2023-07-29 05:34:15 +0300

    separate different event listener ids and implement proper disable()

--- extension.js
+++ extension.js
@@ -22,7 +22,8 @@ const Workspace = imports.ui.workspace.Workspace;
 
 const windowing = extension.imports.windowing;
 
-let eventids = [];
+let wm_eventids = [];
+let display_eventids = [];
 let maximized_windows = [];
 
 let workspace_manager = global.workspace_manager;
@@ -52,7 +53,7 @@ class Extension {
         console.log("Starting Mosaic layout manager.");
         
         let enabled = false; // This prevents recursion
-        eventids.push(global.window_manager.connect(
+        wm_eventids.push(global.window_manager.connect(
             'size-changed', // When size is changed
             (_, win) => {
             if(!enabled) {
@@ -63,7 +64,7 @@ class Extension {
                 
                 if(window.maximized_horizontally === true && window.maximized_vertically === true && windowing.get_all_workspace_windows().length !== 1) {
                     // If maximized (and not alone), move to new workspace and activate it
-                    let new_workspace = windowing.win_to_new_workspace(window);
+                    let new_workspace = windowing.win_to_new_workspace(window, true);
                     /* We mark the window as activated by using its id to index an array
                         We put the value as the active workspace index so that if the workspace anatomy
                         of the current workspace changes, it does not move the maximized window to an unrelated
@@ -71,7 +72,6 @@ class Extension {
                     */
                     maximized_windows[id] = new_workspace.index(); // Mark window as maximized
                     windowing.sort_workspace_windows(workspace); // Sort the workspace where the came from
-                    new_workspace.activate(0);
                     enabled = false;
                     return;
                 } else if(
@@ -87,17 +87,19 @@ class Extension {
             }
         }));
 
-        eventids.push(global.display.connect('window-created', this.sort_window_workspace));
-        eventids.push(global.window_manager.connect('destroy', this.sort_window_workspace_wm));
+        display_eventids.push(global.display.connect('window-created', this.sort_window_workspace));
+        wm_eventids.push(global.window_manager.connect('destroy', this.sort_window_workspace_wm));
 
         // Sort all workspaces at startup
         this.sort_all_workspaces();
     }
 
     disable() {
-        for(let eventid of eventids) {
-            // Disconnect all events
-        }
+        // Disconnect all events
+        for(let eventid of wm_eventids)
+            global.window_manager.disconnect(eventid);
+        for(let eventid of display_eventids)
+            global.display.disconnect(eventid);
     }
 }
 
