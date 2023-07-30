3e3e95c71b3b432d2625acc6e11cd33ba0340fda
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2023-07-30 22:25:10 +0300

    revamp tiling apis

--- tiling.js
+++ tiling.js
@@ -20,7 +20,7 @@ class Tilegroup {
         this.root = root;
         if(!this.root)
             this.root = this;
-        this.id = id + 1;
+        this.id = id;
     }
     check_fit(window) {
         if(this.width + enums.window_spacing + window.width > this.max_width ||
@@ -106,16 +106,13 @@ class Tilegroup {
             x += window.width + enums.window_spacing;
         }
     }
-    get_center_offset(x, y) {
-        return {
-            x: ((this.max_width) / 2) - (this.width / 2) + x,
-            y: ((this.max_height) / 2) - (this.height / 2) + y,
-        }
+    get_center_x_offset(work_area) {
+        return ((work_area.width) / 2) - (this.width / 2) + work_area.x;
     }
 }
 
-function window_descriptor(window, index) {
-    let frame = window.get_frame_rect();
+function window_descriptor(meta_window, index) {
+    let frame = meta_window.get_frame_rect();
 
     this.index = index;
     this.x = frame.x;
@@ -124,23 +121,23 @@ function window_descriptor(window, index) {
     this.height = frame.height;
     this.total_height = frame.height;
     this.total_width = frame.width;
-    this.maximized_horizontally = window.maximized_horizontally;
-    this.maximized_vertically = window.maximized_vertically;
+    this.maximized_horizontally = meta_window.maximized_horizontally;
+    this.maximized_vertically = meta_window.maximized_vertically;
     this.vertical_children = true;
 }
 
-function add_windows(tilegroup, windows, meta_windows, new_window) {
+function add_windows(tilegroup, windows, meta_windows, new_meta_window) {
     for(let window of windows) {
         let status = tilegroup.add_window(window);
         if(status === null && get_all_workspace_windows().length > 1) {
-            if(new_window) {
+            if(new_meta_window) {
                 /* For windows that cannot fit, we move the new window (if applicable) to a new workspace
                     and focus it.
                 */
-                let workspace = windowing.win_to_new_workspace(new_window, false);
+                let workspace = windowing.win_to_new_workspace(new_meta_window, false);
                 let new_windows = windows;
                 for(let i = 0; i < new_windows.length; i++) {
-                    if(meta_windows[new_windows[i].index].get_id() === new_window.get_id()) {
+                    if(meta_windows[new_windows[i].index].get_id() === new_meta_window.get_id()) {
                         new_windows.splice(i, 1);
                         break;
                     }
@@ -156,82 +153,31 @@ function add_windows(tilegroup, windows, meta_windows, new_window) {
     }
 }
 
-function sort_workspace_windows(workspace, move_maximized_windows) {
+function windows_to_descriptors(meta_windows) {
+    let descriptors = [];
+    for(let i = 0; i < meta_windows.length; i++)
+        descriptors.push(new window_descriptor(meta_windows[i], i));
+    return descriptors
+}
+
+function tile_workspace_windows(workspace, reference_meta_window) {
     let meta_windows = workspace.list_windows();
 
     // Put needed window info into an enum so it can be transferred between arrays
-    let window_descriptors = [];
-    for(let i = 0; i < meta_windows.length; i++) {
-        let window = meta_windows[i];
-        // Check if the window is maximized, and move it over if it is
-        if((window.maximized_horizontally === true && window.maximized_vertically === true) && get_all_workspace_windows().length !== 1) {
-            if(move_maximized_windows) // If we are wanting to deal with maximized windows, move them to a new workspace.
-                win_to_new_workspace(window, false);
-            continue; // Skip windows that are maximized otherwise. They will be dealt with by the size-changed listener.
-        }
-        window_descriptors.push(new window_descriptor(window, i));
-    }
-    // Advanced sorter
-    let windows = [];
-    const advanced_sorter = false;
-    if(advanced_sorter) {
-        let vertical = false;
-        while(window_descriptors.length > 0) {
-            let window;
-            let index;
-            if(vertical) {
-                // Get tallest unused window
-                let max = 0;
-                for(let i = 0; i < window_descriptors.length; i++) {
-                    let _window = window_descriptors[i];
-                    if(_window.height > max) {
-                        max = _window.height;
-                        index = i;
-                        window = _window;
-                    }
-                }
-                vertical = false;
-            } else {
-                // Get longest unused window
-                let max = 0;
-                for(let i = 0; i < window_descriptors.length; i++) {
-                    let _window = window_descriptors[i];
-                    if(_window.width > max) {
-                        max = _window.width;
-                        index = i;
-                        window = _window;
-                    }
-                }
-                vertical = true;
-            }
-            windows.push(window);
-            window_descriptors.splice(index, 1);
-        }
-    } else {
-        windows = window_descriptors.sort((a, b) => b.width - a.width);
-    }
+    // Also sort by widest to thinnest
+    let windows = windows_to_descriptors(meta_windows).sort((a, b) => b.width - a.width);
 
-    let n_displays = global.display.get_n_monitors(); // Sort on all monitors
-    for(let i = 0; i < n_displays; i++) {
-        let work_area = workspace.get_work_area_for_monitor(i);
-        // Check for snap tiled windows and adjust work area accordingly
-        for(let i = 0; i < windows.length; i++) {
-            let window = windows[i];
-            if(window.maximized_horizontally === false && window.maximized_vertically === true && windows.length !== 1) {
-                let spaced_width = window.width + enums.window_spacing;
-                if(window.x + window.width === work_area.width)
-                    work_area.width -= spaced_width;
-                if(window.x === work_area.x) {
-                    work_area.x += spaced_width;
-                    work_area.width -= spaced_width;
-                }
-                windows.splice(i, 1);
-                i--;
-            }
-        }
-        let top_bar_height = global.display.get_monitor_geometry(i).height - work_area.height;
-        let root_wingroup = new Tilegroup(work_area.width, work_area.height, false, 0, top_bar_height, -1);
-        root_wingroup.add_windows(windows, meta_windows, workspace.index() === get_workspace().index());
-        root_wingroup.draw_windows(meta_windows, false, root_wingroup.get_center_offset(work_area.x, work_area.y).x);
-    }
+    let current_monitor = reference_meta_window.get_monitor();
+    let work_area = workspace.get_work_area_for_monitor(current_monitor); // Get working area for current space
+
+    const top_bar_height = global.display.get_monitor_geometry(current_monitor).height - work_area.height;
+    let root_wingroup = new Tilegroup(
+        work_area.width,
+        work_area.height - top_bar_height,
+        false,
+        0,
+        top_bar_height,
+        0);
+    add_windows(root_wingroup, windows, meta_windows, reference_meta_window);
+    root_wingroup.draw_windows(meta_windows, false, root_wingroup.get_center_offset(work_area));
 }
