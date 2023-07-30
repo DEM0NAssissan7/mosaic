e5a303b34a2d4d1a85dcdf3c452537a432454936
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2023-07-30 23:55:38 +0300

    create export-zip.sh

---
+++ export-zip.sh
@@ -0,0 +1,20 @@
+uuid="mosaic@mawitime"
+files=(
+    "enums.js"
+    "extension.js"
+    "metadata.json"
+    "tiling.js"
+    "windowing.js"
+)
+
+# Create temporary directory and copy files to it
+mkdir -p "$uuid"
+for file in ${files[@]}; do
+    cp "$file" "$uuid/"
+done
+
+# Export directory to zip
+zip -r "$uuid.zip" "$uuid"
+
+# Remove temporary directory
+rm -r "$uuid"
