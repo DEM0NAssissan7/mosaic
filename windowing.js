9b7ec4b359d6e49f3436fbcab0fedcbbe5ee7b3e
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2023-07-30 20:55:18 +0300

    window switching to workspace respects top bar height

--- windowing.js
+++ windowing.js
@@ -156,10 +156,13 @@ function configure_window(window) {
 
 function win_to_new_workspace(window, switch_to_new) {
     let workspace = global.workspace_manager.append_new_workspace(false, 0); // Create new workspace
-    let active_workspace_index = global.workspace_manager.get_active_workspace_index();
-    global.workspace_manager.reorder_workspace(workspace, active_workspace_index + 1) // Move the new workspace to the right of the current workspace
-    if(switch_to_new) workspace.activate(0);
+    let active_workspace = get_workspace();
+    global.workspace_manager.reorder_workspace(workspace, active_workspace.index() + 1) // Move the new workspace to the right of the current workspace
     window.change_workspace(workspace); // Move window to new workspace
+    let offset = global.display.get_monitor_geometry(window.get_monitor()); // Get top bar offset (if applicable)
+    let frame = window.get_frame_rect();
+    move_window(window, false, 0, offset, frame.width, frame.height - offset);
+    if(switch_to_new) workspace.activate(0);
     return workspace; // Return new workspace
 }
 
