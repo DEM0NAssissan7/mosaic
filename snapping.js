0e85861107622b690dfd8787726cf59d757610f3
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2023-08-05 15:43:49 -0500

    add ability to clear actors

--- snapping.js
+++ snapping.js
@@ -1,11 +1,20 @@
 const st = imports.gi.St;
 const main = imports.ui.main;
 
+var boxes = [];
+
 function rect(x, y, width, height) {
     const box = new st.BoxLayout({ style_class: "feedforward" });
     box.x = x;
     box.y = y;
     box.width = width;
     box.height = height;
+    boxes.push(box);
     main.uiGroup.add_actor(box);
+}
+
+function clear_actors() {
+    for(let box of boxes)
+        main.uiGroup.remove_actor(box);
+    boxes = [];
 }
