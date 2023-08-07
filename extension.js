c20010f9e6a910a8481a122f065e411ecf979e82
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2023-08-07 16:53:33 -0500

    move new window to new workspace if it cannot fit

--- extension.js
+++ extension.js
@@ -60,6 +60,8 @@ class Extension {
     }
 
     created_handler(_, window) {
+        if(!tiling.test_window_fit(window, window.get_workspace(), window.get_monitor()))
+            windowing.move_oversized_window(window);
         setTimeout(() => {
             tile_window_workspace(window);
         }, 100);
