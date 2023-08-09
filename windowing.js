e723c81c3026ec0b052fc832336ce226a1f3fd8a
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2023-08-08 20:25:47 -0500

    use is_on_all_workspaces instead of is_always_on_all_workspaces

--- windowing.js
+++ windowing.js
@@ -116,7 +116,7 @@ function is_excluded(meta_window) {
     if( meta_window.is_hidden() ||
         meta_window.is_attached_dialog() ||
         meta_window.window_type !== 0 ||
-        meta_window.is_always_on_all_workspaces()
+        meta_window.is_on_all_workspaces()
     )
         return true;
     return false;
