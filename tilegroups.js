a67dd3a176186662e60b5508657d46e508c68b24
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2023-08-16 15:40:45 -0500

    initial work on tilegroups

---
+++ tilegroups.js
@@ -0,0 +1,32 @@
+class Tilegroup{
+    constructor(x, y, horizontal) {
+        this.windows = [];
+        this.x = x;
+        this.y = y;
+        this.width = 0;
+        this.height = 0;
+        this.horizontal = horizontal;
+    }
+    add_window(window) {
+
+    }
+    draw() {
+        
+    }
+}
+
+function create_feedforward() {
+    
+}
+
+function create(workspace, windows) {
+
+}
+
+function present(workspace,) {
+
+}
+
+function get_work_area(workspace, monitor) {
+
+}
