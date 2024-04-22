9707caa88216f67f35dd5cece1286e46e9a68c7c
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2024-04-22 13:40:26 +0300

    Restructure extension

--- export-zip.sh
+++ export-zip.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 uuid="window-mosaic-mode@heikkiket"
-files=(
-    "drawing.js"
-    "enums.js"
-    "extension.js"
-    "metadata.json"
-    "reordering.js"
-    "stylesheet.css"
-    "tiling.js"
-    "windowing.js"
-)
-
-# Create temporary directory and copy files to it
-mkdir -p "$uuid"
-for file in ${files[@]}; do
-    cp "$file" "$uuid/"
-done
 
 # Export directory to zip
-zip -r "$uuid.zip" "$uuid"
-
-# Remove temporary directory
-rm -r "$uuid"
+(cd extension && zip -r "../$uuid.zip" .)
