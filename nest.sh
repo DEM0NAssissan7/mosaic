c953e117c5c1d378ab7454b256c28c642a33b3a2
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2024-05-20 14:14:37 -0500

    Change nested GNOME session to 1920x1080

--- nest.sh
+++ nest.sh
@@ -1,8 +1,6 @@
 #!/bin/sh -e
 
 # export G_MESSAGES_DEBUG=all
-export MUTTER_DEBUG_DUMMY_MODE_SPECS=1300x700
+export MUTTER_DEBUG_DUMMY_MODE_SPECS=1920x1080
 
-dbus-run-session -- \
-    gnome-shell --nested \
-                --wayland
+dbus-run-session -- gnome-shell --nested \ --wayland
