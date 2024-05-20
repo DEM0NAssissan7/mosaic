56b25e46b0e64fc7c83ce5c813d746cbfdda3a7b
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2024-05-20 14:15:37 -0500

    clean nest code

--- nest.sh
+++ nest.sh
@@ -3,4 +3,4 @@
 # export G_MESSAGES_DEBUG=all
 export MUTTER_DEBUG_DUMMY_MODE_SPECS=1920x1080
 
-dbus-run-session -- gnome-shell --nested \ --wayland
+dbus-run-session -- gnome-shell --nested --wayland
